f8659c4dafcbe0a87e3d207277415dac612ccfb8
Mustapha Bounoua <bounoua@eurecom.fr>
2026-04-15 16:25:42 +0000

    last

--- soi/setup.sh
+++ soi/setup.sh
@@ -9,12 +9,6 @@ REPO_DIR="${SCRIPT_DIR}/soi_repo"
 echo "=== Installing Python dependencies ==="
 pip install -q -r "${SCRIPT_DIR}/requirements.txt"
 
-echo "=== Cloning SOI repository ==="
-if [ ! -d "${REPO_DIR}" ]; then
-    git clone https://github.com/MustaphaBounoua/soi.git "${REPO_DIR}"
-else
-    echo "Repository already cloned at ${REPO_DIR}"
-fi
 
 echo "=== Setup complete ==="
 echo "You can now run the notebook: soi/notebook.ipynb"
